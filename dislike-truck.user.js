9b7e0a8ceff64ddb60d31bd80fe4aaa24c7c0c1c
nirinium <wills.colton@gmail.com>
2021-04-20 14:51:26 -0400

    Update dislike-truck.user.js

--- dislike-truck.user.js
+++ dislike-truck.user.js
@@ -19,8 +19,8 @@
 
 $(function() {
 
-  var CLIENT_ID = '595110168346-46igp17sotrer74ld1rbg4onc5smse60.apps.googleusercontent.com',
-      API_KEY = 'AIzaSyByJQv-QxraMe7iNFEszkcnNk8JfPRTljY',
+  var CLIENT_ID = '595084133105-ifjuprofcnbvb3ptqu57iflss0fjhia2.apps.googleusercontent.com',
+      API_KEY = 'AIzaSyCszic33z3nUD6xXTlqTxLugnJ7qmVMLBM',
       SCOPES = 'https://www.googleapis.com/auth/youtube',
       PER_PAGE = 50,
       DELAY_TIME = 500,
